e8314d4e053920ebfd8260641a347d53f52ccd05
Fruscie <gilanggsb@gmail.com>
2020-07-31 13:46:31 +0700

    Update config

--- config/Config.js
+++ config/Config.js
@@ -9,7 +9,7 @@ module.exports = {
   //          jika environment variable tidak tersedia, maka nilai APP_PORT menjadi 80
   // APP_PORT: 
       //ubah DB yang sudah di bold dengan host mongodb masing-masing
-      DB: process.env.HOST ? process.env.HOST : 'mongodb+srv://admin:inipassword@gg-6csk1.mongodb.net/learnit-react?retryWrites=true&w=majority',
+      DB: process.env.HOST ? process.env.HOST : 'localhost',
       //learnit-react?retryWrites=true&w=majority',
-      APP_PORT: process.env.APP_PORT? process.env.APP_PORT:80,
+      APP_PORT: process.env.PORT? process.env.PORT:80,
 };
